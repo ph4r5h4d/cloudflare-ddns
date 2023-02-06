f1208f7d25ca77d375084b8987049d9c81d75b7a
Farshad Nematdoust <farshad@nematdoust.com>
2023-02-06 21:45:51 +0100

    Update ipify to use zerolog

--- pkg/ip/current/ipify/ipify.go
+++ pkg/ip/current/ipify/ipify.go
@@ -1,6 +1,7 @@
 package ipify
 
 import (
+  "github.com/rs/zerolog/log"
   "io"
   "net"
   "net/http"
@@ -9,13 +10,17 @@ import (
 type IP net.IP
 
 func (t IP) GetIP() (net.IP, error) {
+  log.Info().Msg("Finding your current IP")
   res, err := http.Get("https://api.ipify.org")
   if err != nil {
     return nil, err
   }
+  
   body, err := io.ReadAll(res.Body)
   if err != nil {
     return nil, err
   }
+  log.Info().Msgf("Your current IP is: '%s'", string(body))
+
   return net.ParseIP(string(body)), nil
 }
